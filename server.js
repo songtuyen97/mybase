be4da6300366dee1f32eaed3a703f38f8911d030
songtuyen <songtuyen97@gmail.com>
2019-09-12 17:18:27 +0700

    Update middleware

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const bodyParse = require('body-parser');
 const middlewareUtil = require('./lib/middleware_Util');
 const controllers = require('./controllers');
 const dbUtil = require('./lib/db_Util');
+const util = require('util');
 // Middleware
 App.use(bodyParse.json());
 App.use(bodyParse.urlencoded({ extended: false }))
@@ -18,6 +19,13 @@ App.use(middlewareUtil.verifyToken);
 App.use(controllers);
 
 let server = http.createServer(App);
+
+const io = require('socket.io')(server);
+io.use(middlewareUtil.accessAndVerifyTokenSocketIO);
+io.on('connection', function(socket) {
+    console.log('socket:' + socket.id);
+    let handshake = socket.handshake;
+})
 
 server.listen(environment.port, function() {
     console.log(`Server is running with port ${environment.port}`);
